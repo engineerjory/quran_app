b0b9bc16f0d5e74a12f36a060369932065885ae4
Eng.jory <eng.jory.1985@gmail.com>
2021-11-14 23:10:28 +0200

    sebha screen and radio screen done

--- lib/models/hadeth_data.dart
+++ lib/models/hadeth_data.dart
@@ -7,14 +7,4 @@ class HadethData {
 
     return hadith;
   }
-  // static Future<String> readSora(int index) async {
-  //   String sora = '';
-  //   List<String> ayat = (await rootBundle.loadString('assets/quran_content/${index + 1}.txt'))
-  //       .trim()
-  //       .split('\n');
-  //   for (int i = 0; i < ayat.length; i++) {
-  //     sora += ayat[i].trim() + ' {${(i + 1)}} ';
-  //   }
-  //   return sora;
-  // }
 }
